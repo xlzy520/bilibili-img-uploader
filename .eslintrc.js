e207301811321b2282e97a53dc48895fb957447f
zhibi <1017866168@qq.com>
2019-11-18 15:47:31 +0800

    update 允许使用未使用的参数，否则表格名称筛选不能生效

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,8 @@ module.exports = {
   extends: ['plugin:vue/essential'],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'off' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'vue/no-unused-vars': 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
