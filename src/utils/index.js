568bb243dfd8bce5553292773fce6e44f2c1d6ee
zhibi <1017866168@qq.com>
2019-11-18 11:12:32 +0800

    opt 优化时间格式化

--- src/utils/index.js
+++ src/utils/index.js
@@ -26,3 +26,35 @@ export const copyToClipboard = function(input) {
   })
   return success
 }
+
+export const parseTime = (time = new Date(), format = '{y}-{m}-{d} {h}:{i}:{s}') =>{
+  let date
+  if (typeof time === 'object') {
+    date = time
+  } else {
+    if ((typeof time === 'string') && (/^[0-9]+$/.test(time))) {
+      time = parseInt(time)
+    }
+    if ((typeof time === 'number') && (time.toString().length === 10)) {
+      time = time * 1000
+    }
+    date = new Date(time)
+  }
+  const formatObj = {
+    y: date.getFullYear(),
+    m: date.getMonth() + 1,
+    d: date.getDate(),
+    h: date.getHours(),
+    i: date.getMinutes(),
+    s: date.getSeconds(),
+    a: date.getDay()
+  }
+  const time_str = format.replace(/{([ymdhisa])+}/g, (result, key) => {
+    const value = formatObj[key]
+    // Note: getDay() returns 0 on Sunday
+    if (key === 'a') { return ['日', '一', '二', '三', '四', '五', '六'][value ] }
+    return value.toString().padStart(2, '0')
+  })
+  return time_str
+}
+
