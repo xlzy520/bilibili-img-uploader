6a40d61e41f13ae5fc210cde25f0b82231064123
xlzy <1017866168@qq.com>
2020-09-01 11:32:28 +0800

    update: 实现热更新开发，用build命令，更改之后重新加载页面就OK

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,19 @@
+const ExtensionReloader = require('webpack-extension-reloader')
+
 module.exports = {
   productionSourceMap: false,
+  runtimeCompiler: true,
+  integrity: true,
+  configureWebpack: {
+    plugins: [
+      new ExtensionReloader()
+    ],
+    resolve: {
+      alias: {
+        vue$: 'vue/dist/vue.runtime.esm.js',
+      },
+    },
+    devtool: false
+  },
+  
 }
