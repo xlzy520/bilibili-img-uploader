d7b23a8e30484f180317debdaa92e8fb1b87cb60
xlzy <1017866168@qq.com>
2020-10-14 17:24:06 +0800

    update: 增加json数据导入功能

--- src/views/home.vue
+++ src/views/home.vue
@@ -12,7 +12,9 @@
       </el-tab-pane>
     </el-tabs>
     <div class="header-button">
-      <wired-button @click="exportJson" elevation="1" type="text" size="small">导出JSON</wired-button>
+      <input type="file" id="importJson" @change="inputChange" hidden/>
+      <wired-button @click="importJson" elevation="1" type="text" size="small">JSON导入</wired-button>
+      <wired-button @click="exportJson" elevation="1" type="text" size="small">JSON导出</wired-button>
     </div>
   </div>
 </template>
@@ -32,7 +34,8 @@
     },
     data() {
       return {
-        activeTab: 'upload'
+        activeTab: 'upload',
+        errNum: 0
       }
     },
     methods: {
@@ -40,6 +43,36 @@
         if (e.name === 'list') {
           this.$refs.list.getImgList()
         }
+      },
+      inputChange(evt){
+        const file = evt.target.files[0];
+        const reader = new FileReader();
+        reader.readAsText(file);
+        reader.onload = (readerEvt) => {
+          const fileString = readerEvt.target.result;
+          const jsonData = JSON.parse(fileString)
+          this.saveDataToIdb(jsonData)
+        }
+      },
+      saveDataToIdb (jsonData=[]){
+        Idb(db_img_config).then(img_db=>{
+          jsonData.forEach(data=>{
+            if (data.name && data.url) {
+              img_db.insert({
+                tableName: "img",
+                data,
+                success: () => console.log("添加成功")
+              });
+            } else {
+              this.errNum += 1
+            }
+          })
+        }).finally(() => {
+          if (this.errNum) {
+            this.$message(this.errNum+'个数据上传失败:','error')
+            this.errNum = 0
+          }
+        })
       },
       exportJson() {
         Idb(db_img_config).then(img_db => {
@@ -59,6 +92,12 @@
             }
           });
         })
+      },
+      importJson(){
+        const input = document.querySelector('#importJson')
+        input.click();
+
+        console.log(input);
       }
     },
   }
