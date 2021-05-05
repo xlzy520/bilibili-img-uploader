7b4dac0c439b3aed572d097dd49531d1d50f2fbe
xlzy <1017866168@qq.com>
2021-05-05 22:23:57 +0800

    fix: 国际化

--- src/main.js
+++ src/main.js
@@ -6,12 +6,12 @@ import defaultConfig from './config.js'
 import { routes } from './router'
 import { createRouter, createWebHashHistory } from 'vue-router'
 
-// import lang from 'element-plus/lib/locale/lang/zh-cn'
-// import 'dayjs/locale/zh-cn'
-// import locale from 'element-plus/lib/locale'
-//
+import lang from 'element-plus/lib/locale/lang/zh-cn'
+import 'dayjs/locale/zh-cn'
+import locale from 'element-plus/lib/locale'
+
 // 设置语言
-// locale.use(lang)
+locale.use(lang)
 
 
 const router = createRouter({
